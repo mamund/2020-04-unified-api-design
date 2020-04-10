d144343353e1d925cdeb1225c25b14e0e7aecc10
Mike Amundsen <mamund@yahoo.com>
2020-04-10 16:43:17 -0400

    update for json and xml output

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@
 const chalk = require("chalk");
 const boxen = require("boxen");
 const yargs = require("yargs");
+const YAML = require("yamljs");
+const { toXML } = require('jstoxml');
 
 const options = yargs
  .usage("Usage: -n <name>")
  .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ .option("t", { alias: "type", describe: "Format Type ( json, xml)", type: "string", demandOption: true})
  .argv;
 
+var alps_yaml = YAML.load(options.name);
+var alps_json = alps_yaml;
+var alps_xml = toXML(alps_json);
+
+if(options.type==="json") {
+  console.log(JSON.stringify(alps_json));
+}
+if(options.type==="xml") {
+  console.log(alps_xml);
+}
+
+/*
 const msg = `Hello, ${options.name}!`;
 
 const greeting = chalk.white.bold(msg);
@@ -23,4 +38,5 @@ const boxenOptions = {
 const msgBox = boxen( greeting, boxenOptions );
 
 console.log(msgBox);
+*/
 
