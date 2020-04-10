0bdb39e7f493a3afda87442d27998c4f7ecce513
Mike Amundsen <mamund@yahoo.com>
2020-04-10 14:37:46 -0400

    update for cli features

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-console.log( "Hello!" );
+const chalk = require("chalk");
+const boxen = require("boxen");
+const yargs = require("yargs");
+
+const options = yargs
+ .usage("Usage: -n <name>")
+ .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ .argv;
+
+const msg = `Hello, ${options.name}!`;
+
+const greeting = chalk.white.bold(msg);
+
+const boxenOptions = {
+	 padding: 1,
+	 margin: 1,
+	 borderStyle: "round",
+	 borderColor: "green",
+	 backgroundColor: "#555555"
+};
+const msgBox = boxen( greeting, boxenOptions );
+
+console.log(msgBox);
+
